23f5614c13e563d14f28d9c0042c6f038929712c
SolnBenchmark <57283557+SolnBenchmark@users.noreply.github.com>
2020-02-19 13:26:56 -0500

    Add files via upload

--- MyMatlab/UGIA_F_2DGauss.m
+++ MyMatlab/UGIA_F_2DGauss.m
@@ -1,7 +1,7 @@
-% [SDV,SDVavg,FWHM,FWHMavg,F,F_]=CRLB_2DGauss(sigma,Kx,Ky,Dx,Dy,Dt,Ih,b,G,xy) 
+% [xyF,F,F_]=UGIA_F_2DGauss(sigma,Kx,Ky,Dx,Dy,Dt,Ih,b,G,xy)
 %
-% Calculate Cramer-Rao lower bound (CRLB) of 2D emitter locations from a 
-% data frame with a Gaussian PSF. 
+% Produce a 2D SOLN image by the UGIA-F estimator and its covariance
+% matrix with a 2D Gaussian PSF
 % 
 % Input:
 % sigma   - SDV of Gaussian PSF 
@@ -14,81 +14,41 @@
 % xy      - ith colume is 2D coordinate (x,y)' of ith emitter location
 %
 % Output:
-% SDV     - ith column of SDV is standard deviation of estimate for ith column of xy
-% SDVavg	- Average SDV of (x,y)
-% FWHM		- FWHM=sqrt(2*log(2))*SDV 
-% FWHMavg	- FWHMavg=sqrt(2*log(2))*SDVavg 
-% F       - Fisher information matrix F
-% F_      - inverse of F
+% xyF     - zeros(2,M), emitter locations estimated by UGIA-F estimator 
+% F       - zero(2*M,2*M), Fisher information matrix 
+% F_      - zero(2*M,2*M), inverse of F 
 %
 % Note: (1) All distances are in nm 
 %       (2) Gaussian noise is approximated by Poisson noise
 %       (3) Gaussian noise mean is ignored
 %
-% Modified from:  CRLB2DGauPSF.m 
-%
 % Reference
 % [1] Sun, Y. Localization precision of stochastic optical localization
 % nanoscopy using single frames. J. Biomed. Optics 18, 111418.1-15 (2013)
+% [2] Y. Sun, "Root mean square minimum distance as a quality metric for
+% stochastic optical localization nanoscopy images," Sci. Reports, vol. 8, 
+% no. 1, pp. 17211, Nov. 2018.
 %
 % Yi Sun
-% 11/16/2019,12/23/2019
+% 02/17/2020
 
-function [SDV,SDVavg,FWHM,FWHMavg,F,F_]=CRLB_2DGauss(sigma,Kx,Ky,Dx,Dy,Dt,Ih,b,G,xy) 
+function [xyF,F,F_]=UGIA_F_2DGauss(sigma,Kx,Ky,Dx,Dy,Dt,Ih,b,G,xy)
 
 [~,M]=size(xy) ;
-if M<1 
-  fprintf(1,'The number of emitters is zero. \n') ;
+if M<1
+  fprintf(1,'# of dyes is zero. \n') ;
   return ;
 end
-r=Ih/(b+G) ;  % total SNR (nm^2/emitter) 
-Q=zeros(Ky,Kx) ; 
-qx=zeros(M,Kx) ;    qy=zeros(M,Ky) ; 
-DqyDy=zeros(M,Ky) ; DqxDx=zeros(M,Kx) ; 
-kx=0:Kx-1 ; ky=0:Ky-1 ; 
+F=Fisher_2DGauss(sigma,Kx,Ky,Dx,Dy,Dt,Ih,b,G,xy) ; 
+F_=pinv(F) ;
+% UGIA-F estimator
+xyF=zeros(2,M) ; 
+[U,L,~]=svd(F_) ;
+W=U*L.^0.5*randn(2*M,1) ;       % error vector achieving Fisher information F
 for i=1:M
-	Dxk1=(Dx*(kx+1)-xy(1,i))/sigma ; Dxk0=(Dx*kx-xy(1,i))/sigma ; 
-	Dyk1=(Dy*(ky+1)-xy(2,i))/sigma ; Dyk0=(Dy*ky-xy(2,i))/sigma ; 
-	qx(i,:)=(Qfunc(-Dxk1)-Qfunc(-Dxk0))/Dx ;
-	qy(i,:)=(Qfunc(-Dyk1)-Qfunc(-Dyk0))/Dy ;
-	Q=Q+qy(i,:)'*qx(i,:) ;
-  DqyDy(i,:)=(1/Dy)*(-exp(-Dyk1.^2/2)+exp(-Dyk0.^2/2))/(sqrt(2*pi)*sigma) ;
-  DqxDx(i,:)=(1/Dx)*(-exp(-Dxk1.^2/2)+exp(-Dxk0.^2/2))/(sqrt(2*pi)*sigma) ;
+  i1=2*(i-1)+1 ; i2=2*(i-1)+2 ; 
+  error=W(i1:i2) ;              % error vector for ith emitter
+  xyF(:,i)=xy(:,i)+error ;      % location estimated by UGIA_F estimator
 end
-Qu=Q+1/r ;      % Gaussian noise is approximated as Poisson noise 
-if r==inf 
-  Qu=Qu+1e-10 ; % add 1e-10 to avoid 0/0=NaN 
-end
-F=zeros(2*M,2*M) ;	% Fisher information matrix of (x1,y1,x2,y2,...,xM,yM)' 
-for i=1:M	% 
-	qyDqxDxi=qy(i,:)'*DqxDx(i,:) ; 
-	DqyDyqxi=DqyDy(i,:)'*qx(i,:) ;
-	for j=i:M
-		qyDqxDxj=qy(j,:)'*DqxDx(j,:) ;
-		DqyDyqxj=DqyDy(j,:)'*qx(j,:) ;
-% F(xi,:)
-		F(2*(i-1)+1,2*(j-1)+1)=sum(sum(qyDqxDxi.*qyDqxDxj./Qu)) ;	% F(xi, xj)
-		F(2*(j-1)+1,2*(i-1)+1)=F(2*(i-1)+1,2*(j-1)+1) ;						% F(xj, xi)
-		F(2*(i-1)+1,2*(j-1)+2)=sum(sum(qyDqxDxi.*DqyDyqxj./Qu)) ;	% F(xi, yj)
-		F(2*(j-1)+2,2*(i-1)+1)=F(2*(i-1)+1,2*(j-1)+2) ;						% F(yj, xi)
-% F(yi,:)
-		F(2*(i-1)+2,2*(j-1)+1)=sum(sum(DqyDyqxi.*qyDqxDxj./Qu)) ;	% F(yi, xj)
-		F(2*(j-1)+1,2*(i-1)+2)=F(2*(i-1)+2,2*(j-1)+1) ;						% F(xj, yi)
-		F(2*(i-1)+2,2*(j-1)+2)=sum(sum(DqyDyqxi.*DqyDyqxj./Qu)) ;	% F(yi, yj)
-		F(2*(j-1)+2,2*(i-1)+2)=F(2*(i-1)+2,2*(j-1)+2) ;						% F(yj, yi)
-	end
-end
-F=Ih*Dt*Dx*Dy*F ; F_=inv(F) ; 
-var=diag(F_) ;
-SDV=zeros(2,M) ;
-for i=1:M
-	SDV(1,i)=sqrt(var(2*(i-1)+1)) ;	% SDV for x
-	SDV(2,i)=sqrt(var(2*(i-1)+2)) ;	% SDV for y	
-end
-SDVavg=zeros(2,1) ;
-SDVavg(1)=sum(SDV(1,:))/M ;
-SDVavg(2)=sum(SDV(2,:))/M ;
-FWHM=2*sqrt(2*log(2))*SDV ;
-FWHMavg=2*sqrt(2*log(2))*SDVavg ;
 
 end
