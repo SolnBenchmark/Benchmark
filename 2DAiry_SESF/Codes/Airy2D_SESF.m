515293205f70e84413869d08376dc55689e5437a
SolnBenchmark <57283557+SolnBenchmark@users.noreply.github.com>
2020-04-03 13:12:52 -0400

    Add files via upload

--- 2DAiry_SESF/Codes/Airy2D_SESF.m
+++ 2DAiry_SESF/Codes/Airy2D_SESF.m
@@ -21,7 +21,7 @@
 % Electrical Engineering Department
 % The City College of City University of New York
 % E-mail: ysun@ccny.cuny.edu
-% 11/23/2019, 02/22/2020
+% 11/23/2019, 02/22/2020, 04/02/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
@@ -34,30 +34,32 @@ fprintf(1,'%s: \n',SNRr) ;
 rng('default') ; 
 key=0 ;             % key for random number generators
 switch SNRr
-  case 'highSNR'    % r=1/(1/rp+1/rg), 10*log10(r)=54.77 (dB)
-    b=0.5 ;         % rp=Ih/b, 10*log10(rp)=57.78 (dB)
-    G=0.5 ;         % rg=Ih/G, 10*log10(rg)=57.78 (dB)
+  case 'highSNR'    % r=500000,   SNR=56.99 (dB)
+    b=0.3 ;         % rp=1000000, SPNR=60.00 (dB)
+    G=0.3 ;         % rg=1000000, SGNR=60.00 (dB)
     key=key+0 ;  
-  case 'mediumSNR'  % r=1/(1/rp+1/rg), 10*log10(r)=40.79 (dB)
-    b=15 ;          % rp=Ih/b, 10*log10(rp)=43.01 (dB)
-    G=10 ;          % rg=Ih/G, 10*log10(rg)=44.77 (dB)
-    key=key+1 ;  
-  case 'lowSNR'     % r=1/(1/rp+1/rg), 10*log10(r)=37.78 (dB)
-    b=30 ;          % rp=Ih/b, 10*log10(rp)=40.00 (dB)
-    G=20 ;          % rg=Ih/G, 10*log10(rg)=41.76 (dB)
-    key=key+2 ;  
+  case 'mediumSNR'  % r=37500,    SNR=45.74 (dB)
+    b=5 ;           % rp=60000,   SPNR=47.78 (dB)
+    G=3 ;           % rg=100000,  SGNR=50.00 (dB)
+    key=key+1 ; 
+  case 'lowSNR'     % r=9375,     SNR=39.72 (dB)
+    b=20 ;          % rp=15000,   SPNR=41.76 (dB)
+    G=12 ;          % rg=25000,   SGNR=43.98 (dB)
+    key=key+2 ; 
   otherwise
     return ;
 end
 rng(key) ; 
 %% Optical system 
-na=1.4 ; 
-lambda=520 ;          % nm
-a=2*pi*na/lambda ; 
-sigma=1.3238/a ;      % = 78.26 nm, SD of Airy PSF
+na=1.43 ; 
+lambda=665 ;                  % Alexa647 wavelength in nm
+a=2*pi*na/lambda ;  
+% 2D Gaussian PSF; sigma is estimated from Airy PSF
+sigma=1.3238/a ;              % sigma=97.98; 2*sigma=195.96 (nm) 
+FWHM=2*sqrt(2*log(2))*sigma ; % FWHM=230.72 (nm)
 %% Frame 
 % Region of view: [0,Lx]x[0,Ly]
-Lx=25e3 ; Ly=25e3 ;   % frame size in nm
+Lx=31400 ; Ly=31400 ; % frame size in nm
 Dx=100 ; Dy=100 ;     % pixel size of cammera
 Kx=Lx/Dx ; Ky=Ly/Dy ; % frame size in pixels
 %% Emitter intensity and signal to noise ratio
@@ -70,7 +72,8 @@ rg=Ih/G ;             % SGNR (nm^2/emitter)
 SGNR=10*log10(rg) ;   % SGNR (dB)
 r=rp*rg/(rp+rg) ;     % total SNR (nm^2/emitter) 
 SNR=10*log10(r) ;     % total SNR (dB)
-mu=0.5 ;              % mean of Gaussian noise (photons/s/nm^2)
+mu=5 ;                % mean of Gaussian noise (photons/s/nm^2)
+Coff=mu*Dt*Dx*Dy ;    % Coff=500 photons/pixel; Camera offset in effect
 %% Emitter locations - ground truth
 eLx=2*5*sigma ;       % an emitter is located in a region of size eLx, eLy
 eLy=2*5*sigma ;       % with probability of 99% 
@@ -114,9 +117,16 @@ xy_=xy1' ;
 [RMSMD1,RMSMD2]=RMSMD(xy_,xyF) ;
 fprintf(1,SNRr) ; 
 fprintf(1,': SNR=%5.2f (dB) M=%d RMSMD=%6.3f (nm) \n',SNR,M,RMSMD1) ; 
+%% Show estimates
+figure('Position',[400 300 600 600],'Color',[1 1 1]) ;
+plot(xy_(1,:),xy_(2,:),'k.') ; hold on
+plot(xyF(1,:),xyF(2,:),'r.') ; hold off
+xlabel('x (nm)') 
+ylabel('y (nm)') ; 
+axis([0 Lx 0 Ly])
 
-%% Results, M=961: UGIA-F estimator
-%           highSNR   mediumSNR   lowSNR    Average 
-% SNR:      54.77     40.79       37.78 (dB) 
-% RMSMD:    3.08      9.48        12.98     8.51 (nm) 
-% Average: mean([3.08 9.48 12.98])=8.51 (nm)
+%% Results, M=1024: UGIA-F estimator
+%             highSNR mediumSNR lowSNR  Average 
+% SNR (dB):   56.99   45.74     39.72 
+% RMSMD (nm): 3.75    8.53      15.86   9.38
+% Average: mean([3.75    8.53      15.86]) 
