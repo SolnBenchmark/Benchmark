dcb667022d8ef99b40486d46c4d695c13f47227c
SolnBenchmark <57283557+SolnBenchmark@users.noreply.github.com>
2020-04-05 22:07:09 -0400

    Add files via upload

--- 2DAiry_MEMF_MTR/Codes/Airy2D_MEMF_MTR.m
+++ 2DAiry_MEMF_MTR/Codes/Airy2D_MEMF_MTR.m
@@ -26,7 +26,7 @@
 % Electrical Engineering Department
 % The City College of City University of New York
 % E-mail: ysun@ccny.cuny.edu
-% 02/26/2020
+% 02/26/2020, 04/05/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
@@ -41,10 +41,12 @@ key=key+eD ;
 rng(key) ; 
 fprintf(1,'Emitter distance: %d (nm) \n',eD) ; 
 %% Optical system 
-na=1.4 ; 
-lambda=520 ;          % nm
-a=2*pi*na/lambda ; 
-sigma=1.3238/a ;      % = 78.26 nm
+na=1.43 ; 
+lambda=665 ;                  % Alexa647 wavelength in nm
+a=2*pi*na/lambda ;  
+% 2D Gaussian PSF; sigma is estimated from Airy PSF
+sigma=1.3238/a ;              % sigma=97.98; 2*sigma=195.96 (nm) 
+FWHM=2*sqrt(2*log(2))*sigma ; % FWHM=230.72 (nm)
 %% Frame 
 % Region of view: [0,Lx]x[0,Ly]
 Lx=2^11 ;
@@ -55,16 +57,17 @@ Kx=Lx/Dx ; Ky=Ly/Dy ; % frame size in pixels
 Dt=0.01 ;             % second, time per frame (1/Dt is frame rate) 
 Ih=300000 ;           % average number of detected photons per emitter per second
 DtIh=Dt*Ih ;          % photon count per frame per emitter 
-% 'mediumSNR'         % r=1/(1/rp+1/rg), 10*log10(r)=40.79 (dB)
-b=15 ;                % rp=Ih/b, 10*log10(rp)=43.01 (dB)
-G=10 ;                % rg=Ih/G, 10*log10(rg)=44.77 (dB)
+% 'mediumSNR'         % r=37500,    SNR=45.74 (dB)
+b=5 ;                 % rp=60000,   SPNR=47.78 (dB)
+G=3 ;                 % rg=100000,  SGNR=50.00 (dB)
 rp=Ih/b ;             % SPNR (nm^2/emitter) 
 SPNR=10*log10(rp) ;   % SPNR (dB)
 rg=Ih/G ;             % SGNR (nm^2/emitter) 
 SGNR=10*log10(rg) ;   % SGNR (dB)
 r=rp*rg/(rp+rg) ;     % total SNR (nm^2/emitter) 
 SNR=10*log10(r) ;     % total SNR (dB)
-mu=0.5 ;              % mean of Gaussian noise (photons/s/nm^2)
+mu=5 ;                % mean of Gaussian noise (photons/s/nm^2)
+Coff=mu*Dt*Dx*Dy ;    % Coff=500 photons/pixel; Camera offset in effect
 %% Emitter locations - ground truth
 fprintf(1,'Emitter locations: \n') ;
 M=250 ; 
@@ -263,7 +266,7 @@ xlabel('Temporal resolution (s)') ;
 % pM: # emitters activated at least once in a movie
 % Distance: 40    30    20    Average (nm)
 % pM:       250   250   250 
-% UGIA-M:   3.29  2.97  3.51  3.26
-% UGIA-F:   11.10 11.61 17.62 13.44
-% Average RMSMD-M: mean([3.29  2.97  3.51])
-% Average RMSMD-F: mean([11.10 11.61 17.62])
+% UGIA-M:   2.77  2.77  3.06  2.87
+% UGIA-F:   10.49 12.70 18.14 13.78
+% Average RMSMD-M: mean([2.77  2.77  3.06])
+% Average RMSMD-F: mean([10.49 12.70 18.14])
