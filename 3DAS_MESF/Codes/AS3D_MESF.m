0adf147ec3ebc0191a1cef74500217bfedbd84bf
SolnBenchmark <57283557+SolnBenchmark@users.noreply.github.com>
2020-04-07 17:16:15 -0400

    Add files via upload

--- 3DAS_MESF/Codes/AS3D_MESF.m
+++ 3DAS_MESF/Codes/AS3D_MESF.m
@@ -21,7 +21,7 @@
 % Electrical Engineering Department
 % The City College of City University of New York
 % E-mail: ysun@ccny.cuny.edu
-% 03/01/2019
+% 03/01/2019, 04/07/2020
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
 clear
@@ -56,9 +56,9 @@ legend(lg,'\sigma_x(z)','\sigma_y(z)','Location','southeast')
 xlabel('z (nm)')
 ylabel('\sigma_x(z), \sigma_y(z) (nm)')
 %% Frame 
-M=500 ;              % number of emitters 
+M=500 ;               % number of emitters 
 % sample is located at [0,Lx]x[0,Ly]x[-Lz,Lz]
-Lx=1e3*round(sqrt(M/eDen)) ;
+Lx=100*round(10*sqrt(M/eDen)) ; % an integer multiple of Dx
 Ly=Lx ;               % frame size in nm
 Dx=100 ; Dy=100 ;     % pixel size of cammera
 Kx=Lx/Dx ; Ky=Ly/Dy ; % frame size in pixels
@@ -67,16 +67,17 @@ Lz=400 ;              % 2xLz - axial depth in nm
 Dt=0.01 ;             % second, time per frame (1/Dt is frame rate) 
 Ih=300000 ;           % average number of detected photons per emitter per second
 DtIh=Dt*Ih ;          % photon count per frame per emitter 
-% 'mediumSNR'         % r=1/(1/rp+1/rg), 10*log10(r)=57.78 (dB)
-b=0.3 ;               % rp=Ih/b, 10*log10(rp)=60.00 (dB)
-G=0.2 ;               % rg=Ih/G, 10*log10(rg)=61.76 (dB)
+% 'mediumSNR'         % r=600000,   SNR=57.78 (dB)
+b=0.3 ;               % rp=1000000,  SPNR=60.00 (dB)
+G=0.2 ;               % rg=1500000, SGNR=61.76 (dB)
 rp=Ih/b ;             % SPNR (nm^2/emitter) 
 SPNR=10*log10(rp) ;   % SPNR (dB)
 rg=Ih/G ;             % SGNR (nm^2/emitter) 
 SGNR=10*log10(rg) ;   % SGNR (dB)
 r=rp*rg/(rp+rg) ;     % total SNR (nm^2/emitter) 
 SNR=10*log10(r) ;     % total SNR (dB)
-mu=0.5 ;              % mean of Gaussian noise (photons/s/nm^2)
+mu=5 ;                % mean of Gaussian noise (photons/s/nm^2)
+Coff=mu*Dt*Dx*Dy ;    % Coff=500 photons/pixel; Camera offset in effect
 %% Emitter locations - ground truth
 xyz=[Lx*rand(1,M)     % randomly uniformly distributed in [0,Lx]x[0,Ly]x[-Lz,Lz]
      Ly*rand(1,M)
@@ -84,8 +85,6 @@ xyz=[Lx*rand(1,M)     % randomly uniformly distributed in [0,Lx]x[0,Ly]x[-Lz,Lz]
 xyz0=xyz' ;           % ground truth emitter locaitons 
 filename_xyz0=strcat('3DAS_MESF_density',num2str(eDen),'_xyz0','.txt') ; 
 save(filename_xyz0,'-ascii','xyz0') ;
-figure 
-plot3(xyz(1,:),xyz(2,:),xyz(3,:),'r*') ; 
 %% Generate and save a data frame
 fprintf(1,'Generate a data frame: \n') ; 
 U=AS3D_Frame(c,d,sigmax0,Ax,Bx,sigmay0,Ay,By,Kx,Ky,Dx,Dy,Dt,Ih,b,mu,G,xyz) ;
@@ -113,9 +112,17 @@ xyz_=xyz1' ;
 %% Calculate RMSMD
 [RMSMD1,RMSMD2]=RMSMD(xyz_,xyzF) ;
 fprintf(1,'Density=%2.1f Lx=%d Ly=%d M=%d RMSMD=%6.3f (nm) \n',eDen,Lx,Ly,M,RMSMD1) ; 
+%% Show estimates
+figure('Position',[400 300 600 600],'Color',[1 1 1]) ;
+plot3(xyz_(1,:),xyz_(2,:),xyz_(3,:),'k.') ; hold on
+plot3(xyzF(1,:),xyzF(2,:),xyzF(3,:),'r.') ; hold off
+xlabel('x (nm)') 
+ylabel('y (nm)')
+ylabel('z (nm)') ; 
+axis([0 Lx 0 Ly -Lz Lz])
 
 %% Results, M=500: UGIA-F estimator
-% eDen      0.1   0.3   0.5   1     2     3     Average 
-% Lx=Ly     71e3  41e3  32e3  22e3  16e3  13e3
-% RMSMD     18.07 22.65 22.37 27.57 38.61 69.41 57.78 (nm) 
-% mean([18.07 22.65 22.37 27.57 38.61 69.41])=57.78 
+% eDen        0.1   0.3   0.5   1     2     3     Average 
+% Lx=Ly (nm)  70700 40800 31600 22400 15800 12900
+% RMSMD (nm)  16.90 21.44 22.51 25.37 42.85 68.90 33.00
+% mean([16.90 21.44 22.51 25.37 42.85 68.90])
